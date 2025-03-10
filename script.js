4c7496413e0d6dc311d25a643d94276e7b65621c
Gokul Anand S <sgokulanand0505@gmail.com>
2025-03-10 23:03:48 +0530

    updated

---
+++ script.js
@@ -0,0 +1,34 @@
+function redirectHome(){
+    window.location.href="LandingPage.html";
+}
+function redirectReport(){
+    window.location.href="IncidentReportForm.html";
+}
+
+function redirectEmergencyAlerts(){
+    window.location.href="EmergencyAlerts.html";
+}
+function redirectDisGuide(){
+    window.location.href="DisGuide.html";
+}
+function redirectLocRes(){
+    window.location.href="LocalResource.html";
+}
+
+
+document.querySelector('.button-emKit').addEventListener('click', function() {
+    document.getElementById('modal').classList.add('active');
+});
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.getElementById('modal').classList.remove('active');
+});
+
+
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+function hidePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
